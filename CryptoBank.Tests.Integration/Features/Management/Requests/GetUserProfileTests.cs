0442fa58e8cd847f01b3286f2ac655108b75d724
Салих Олжас <osalikh@dm.findexit.kz>
2023-10-11 00:29:54 +0600

    User Profile Tests Complite

--- CryptoBank.Tests.Integration/Features/Management/Requests/GetUserProfileTests.cs
+++ CryptoBank.Tests.Integration/Features/Management/Requests/GetUserProfileTests.cs
@@ -1,9 +1,11 @@
 ﻿using CryptoBank.Common.Passwords;
-using CryptoBank.Features.Management.Domain;
 using CryptoBank.Features.Management.Requests;
+using CryptoBank.Tests.Integration.Errors.Contracts;
 using CryptoBank.Tests.Integration.Fixtures;
 using CryptoBank.Tests.Integration.Helpers;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
@@ -27,23 +29,7 @@ public class GetUserProfileTests : IAsyncLifetime
         // Arrange
         var passwordHasher = _scope.ServiceProvider.GetRequiredService<Argon2IdPasswordHasher>();
 
-        var user = new User
-        {
-            Email = "example@example.com",
-            Password = passwordHasher.HashPassword("qwerty123456A!"),
-            DateOfBirth = new DateTime(2000, 01, 31).ToUniversalTime(),
-            DateOfRegistration = DateTime.UtcNow,
-            UserRoles = new List<UserRole>()
-            {
-                new()
-                {
-                    Role = new Role
-                    {
-                        Name = "User", Description = "Обычный пользователь"
-                    }
-                }
-            }
-        };
+        var user = UserHelper.CreateUser("me@example.com", passwordHasher.HashPassword("12345678"));
 
         await _fixture.Database.Execute(async x =>
         {
@@ -58,6 +44,55 @@ public class GetUserProfileTests : IAsyncLifetime
 
         // Act
         var response = await client.GetFromJsonAsync<GetUserProfile.Response>("/profile");
+
+        // Assert
+        response.Should().NotBeNull();
+        response!.Id.Should().Be(user.Id);
+        response.Email.Should().Be(user.Email);
+        response.DateOfBirth.Date.Should().Be(user.DateOfBirth.Date);
+        response.DateOfRegistration.Date.Should().Be(user.DateOfRegistration.Date);
+    }
+
+    [Fact]
+    public async Task Should_invalid_token()
+    {
+        // Arrange
+        var jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwia";
+
+        var client = _fixture.HttpClient.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+
+        // Act
+        var response = await client.GetAsync("/profile");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Should_user_not_found()
+    {
+        // Arrange
+        var passwordHasher = _scope.ServiceProvider.GetRequiredService<Argon2IdPasswordHasher>();
+
+        var user = UserHelper.CreateUser("me@example.com", passwordHasher.HashPassword("12345678"));
+
+        var jwt = AuthenticateHelper.GetAccessToken(user, _scope);
+
+        var client = _fixture.HttpClient.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+
+        // Act
+        var response = await client.GetAsync("/profile");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var validationProblemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetailsContract>();
+        validationProblemDetails.Should().NotBeNull();
+
+        var error = validationProblemDetails!.Errors.Single();
+        error.Code.Should().Be("user_profile_validation_user_not_found");
     }
 
     public async Task InitializeAsync()
