821706c531e2a2297b9d64f85434efffca786f05
Салих Олжас <osalikh@dm.findexit.kz>
2023-10-23 00:19:30 +0600

    Password Authentication Tests Complite

---
+++ CryptoBank.Tests.Integration/Features/AuthTestsCollection.cs
@@ -0,0 +1,13 @@
+﻿using CryptoBank.Tests.Integration.Fixtures;
+
+namespace CryptoBank.Tests.Integration.Features;
+
+[CollectionDefinition(Name)]
+public class AuthTestsCollection : ICollectionFixture<TestFixture>
+{
+    public const string Name = nameof(AuthTestsCollection);
+
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
---
+++ CryptoBank.Tests.Integration/Helpers/ValidationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CryptoBank.Tests.Integration.Errors.Contracts;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+
+namespace CryptoBank.Tests.Integration.Helpers;
+
+public static class ValidationExtensions
+{
+    public static void ShouldContain(this ValidationProblemDetailsContract contract, string expectedField,
+        string expectedMessage, string expectedCode)
+    {
+        contract.Should().NotBeNull();
+        contract.Title.Should().Be("Validation failed");
+        contract.Type.Should().Be("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400");
+        contract.Detail.Should().Be("One or more validation errors have occurred");
+        contract.Status.Should().Be(StatusCodes.Status400BadRequest);
+        contract.Errors.Should().HaveCount(1);
+
+        var error = contract.Errors.Single();
+        error.Field.Should().Be(expectedField);
+        error.Message.Should().Be(expectedMessage);
+        error.Code.Should().Be(expectedCode);
+    }
+}
