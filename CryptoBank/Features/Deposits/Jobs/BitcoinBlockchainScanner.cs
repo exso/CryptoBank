49ba36c6b46ed21b0aa2ac168fd79b9a6f4a3da0
Салих Олжас <osalikh@dm.findexit.kz>
2023-11-29 17:16:14 +0600

    Bitcoin Deposits Saving V3

--- CryptoBank/Features/Deposits/Jobs/BitcoinBlockchainScanner.cs
+++ CryptoBank/Features/Deposits/Jobs/BitcoinBlockchainScanner.cs
@@ -42,51 +42,20 @@ public class BitcoinBlockchainScanner : BackgroundService
 
                 var context = scope.ServiceProvider.GetRequiredService<Context>();
 
-                var network = _bitcoinNetworkService.GetNetwork();
-
                 await using var tx = await context.Database.BeginTransactionAsync(IsolationLevel.RepeatableRead,
                     cancellationToken);
-
-                var depositAddress = await context.DepositAddresses.ToArrayAsync(cancellationToken);
-
-                await SaveLatestBlock(context, cancellationToken);
 
                 var latestBlock = await GetLatestBlock(context, cancellationToken);
 
                 var currentBlock = await _client.GetBlockCountAsync(cancellationToken);
 
-                for (var h = latestBlock; h <= currentBlock; h++)
-                {
-                    var block = await _client.GetBlockAsync(h, cancellationToken);
+                await ScanBlocks(latestBlock, currentBlock, context, cancellationToken);
 
-                    var txOuts = from t in block.Transactions.SelectMany(t => t.Outputs)
-                                 select new
-                                 {
-                                     TxId = string.Empty,
-                                     Address = t.ScriptPubKey.GetDestinationAddress(network)?.ToString(),
-                                     t.Value
-                                 };
+                await context.Variables
+                    .Where(x => x.Key == BitcoinBlock.Key)
+                    .ExecuteUpdateAsync(s => s
+                        .SetProperty(x => x.Value, currentBlock), cancellationToken);
 
-                    var deposits = from t in txOuts
-                                   join d in depositAddress on t.Address equals d.CryptoAddress
-                                   select new CryptoDeposit
-                                   {
-                                       UserId = d.UserId,
-                                       AddressId = d.Id,
-                                       Amount = t.Value.ToDecimal(MoneyUnit.BTC),
-                                       CurrencyId = d.CurrencyId,
-                                       CreatedAt = DateTime.UtcNow,
-                                       TxId = t.TxId,
-                                       Status = DepositStatus.Created
-                                   };
-
-                    if (deposits.Any())
-                    {
-                        await context.CryptoDeposits.AddRangeAsync(deposits, cancellationToken);
-                        await context.SaveChangesAsync(cancellationToken);
-                    }                
-                }
-                
                 await tx.CommitAsync(cancellationToken);
             }
             catch (Exception ex)
@@ -98,25 +67,71 @@ public class BitcoinBlockchainScanner : BackgroundService
         }
     }
 
-    private async Task SaveLatestBlock(Context context, CancellationToken cancellationToken)
-    {
-        var latestBlock = await _client.GetBlockCountAsync(cancellationToken);
-
-        var variable = new Variable
-        {
-            Key = BitcoinBlock.Key,
-            Value = latestBlock
-        };
-
-        await context.Variables.AddAsync(variable, cancellationToken);
-        await context.SaveChangesAsync(cancellationToken);
-    }
-
-    private static async Task<int> GetLatestBlock(Context context, CancellationToken cancellationToken)
+    private async Task<int> GetLatestBlock(Context context, CancellationToken cancellationToken)
     {
         var latestBlock = await context.Variables
-            .SingleAsync(x => x.Key == BitcoinBlock.Key, cancellationToken);
+            .SingleOrDefaultAsync(x => x.Key == BitcoinBlock.Key, cancellationToken);
+
+        if (latestBlock == null)
+        {
+            var latestBlockCount = await _client.GetBlockCountAsync(cancellationToken);
+
+            var variable = new Variable
+            {
+                Key = BitcoinBlock.Key,
+                Value = latestBlockCount
+            };
+
+            await context.Variables.AddAsync(variable, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return variable.Value;
+        }
 
         return latestBlock.Value;
+    }
+
+    private async Task ScanBlocks(
+        int latestBlock, 
+        int currentBlock, 
+        Context context, 
+        CancellationToken cancellationToken)
+    {
+        var depositAddress = await context.DepositAddresses.ToArrayAsync(cancellationToken);
+
+        var network = _bitcoinNetworkService.GetNetwork();
+
+        for (var h = latestBlock; h <= currentBlock; h++)
+        {
+            var block = await _client.GetBlockAsync(h, cancellationToken);
+
+            var txOuts = from t in block.Transactions
+                         from o in t.Outputs
+                         select new
+                         {
+                             TxId = t.GetHash().ToString(),
+                             Address = o.ScriptPubKey.GetDestinationAddress(network)?.ToString(),
+                             Amount = o.Value
+                         };
+
+            var deposits = from t in txOuts
+                           join d in depositAddress on t.Address equals d.CryptoAddress
+                           select new CryptoDeposit
+                           {
+                               UserId = d.UserId,
+                               AddressId = d.Id,
+                               Amount = t.Amount.ToDecimal(MoneyUnit.BTC),
+                               CurrencyId = d.CurrencyId,
+                               CreatedAt = DateTime.UtcNow,
+                               TxId = t.TxId,
+                               Status = DepositStatus.Created
+                           };
+
+            if (deposits.Any())
+            {
+                await context.CryptoDeposits.AddRangeAsync(deposits, cancellationToken);
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        }
     }
 }
